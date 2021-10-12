18678f26867b56c954035360d430617924becaf0
Igor Vasilev <Protanker25@gmail.com>
2021-10-12 23:04:08 +1000

    Class Player

--- Assets/Scripts/Cage.cs
+++ Assets/Scripts/Cage.cs
@@ -1,4 +1,4 @@
-using System.Drawing;
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +7,21 @@ public class Cage : MonoBehaviour
 {
     public int Id;
     public int OwnerId;
-    public Point coordinates;
+    [SerializeField]public Point coordinates;
 
     public Cage(int Id, int OwnerId, Point coordinates){
         this.Id = Id;
         this.OwnerId = OwnerId;
         this.coordinates = coordinates;
     }
-    
+   
+}
+[System.Serializable]
+public struct Point{
+    public int x;
+    public int y;
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
 }
